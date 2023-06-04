e471c29dd19c924f986980b64299f5a451612211
erikhag1 <erik.hagen@digdir.no>
2023-06-05 00:55:04 +0200

    Dev

---
+++ wiki/playbook/update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Updating component messaging..."
+echo "=========================="
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/components/messaging
+git checkout main
+git add .
+git commit -m "Dev"
+git push origin main
+echo ""
+echo ""
+echo ""
+echo "Updating playbook..."
+echo "===================="
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
+git checkout main
+git add .
+git commit -m "Dev"
+git push origin main
+echo ""
+echo ""
+echo "Running Antora..."
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
+npx antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_editing=0 --attribute target_audience=all --attribute include_early_drafts=2
+echo "Updating site..."
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/output/altinn.github.io/ark/docs
+touch .nojekyll
+#git checkout master
+#git add .
+#git commit -m "Dev"
+#git push origin master
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
