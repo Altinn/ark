d92ef63aa251791695fae52f7b695e8ab94bd770
erikhag1 <erik.hagen@digdir.no>
2023-06-05 12:29:07 +0200

    Dev

--- wiki/playbook/update.sh
+++ wiki/playbook/update.sh
@@ -22,7 +22,7 @@ echo "Running Antora..."
 cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
 npx antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_editing=0 --attribute target_audience=all --attribute include_early_drafts=2
 echo "Updating site..."
-cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/output/altinn.github.io/ark/docs
+cd /c/Users/eha/OneDrive/antora/altinn/ark/docs
 touch .nojekyll
 #git checkout master
 #git add .
