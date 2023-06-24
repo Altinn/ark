6e3828cda4bf620126ceb503e866c15d6636dc69
erikhag1 <erik.hag1@gmail.com>
2023-06-25 00:17:13 +0200

    Dev

--- wiki/playbook/update.sh
+++ wiki/playbook/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Updating component messaging..."
 echo "=========================="
-cd /c/Users/eha/OneDrive/antora/altinn3/ark/wiki/components/messaging
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/components/messaging
 git checkout main
 git add .
 git commit -m "Dev"
@@ -11,7 +11,7 @@ echo ""
 echo ""
 echo "Updating playbook..."
 echo "===================="
-cd /c/Users/eha/OneDrive/antora/altinn3/ark/wiki/playbook
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
 git checkout main
 git add .
 git commit -m "Dev"
@@ -19,13 +19,13 @@ git push origin main
 echo ""
 echo ""
 echo "Running Antora..."
-cd /c/Users/eha/OneDrive/antora/altinn3/ark/wiki/playbook
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
 npx antora --fetch antora-playbook.yml --attribute lang=no 	--attribute wysiwig_editing=0 --attribute target_audience=all --attribute include_early_drafts=2
 echo "Updating site..."
-cd /c/Users/eha/OneDrive/antora/altinn3/ark/docs
+cd /c/Users/eha/OneDrive/antora/altinn/ark/docs
 touch .nojekyll
 #git checkout main
 #git add .
 #git commit -m "Dev"
 #git push origin main
-cd /c/Users/eha/OneDrive/antora/altinn3/ark/wiki/playbook
+cd /c/Users/eha/OneDrive/antora/altinn/ark/wiki/playbook
